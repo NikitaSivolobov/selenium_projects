747d6c6c0ab88e2484f7cb4cd5647daa8f19d32c
nikita1314 <nikita1314@yandex.ru>
2022-06-03 00:14:23 +0300

    Вариация на тест по проверки поиска в Яндексе ссылки на tensor.ru в результатах поиска

--- selenium_projects/pages/yandex_page.py
+++ selenium_projects/pages/yandex_page.py
@@ -19,11 +19,8 @@ class YandexPage(BasePage):
     def tensor_search(self):
         self.browser.find_element(*YandexLocators.SEARCH_FIELD).send_keys(Keys.ENTER)
         result_links = self.browser.find_elements(By.XPATH, '//div[@class="VanillaReact OrganicTitle OrganicTitle_wrap Typo Typo_text_l Typo_line_m organic__title-wrapper"]/a')
-        for variant in result_links:
-            item_link = variant.get_attribute('href')
-            list_item = []
-            list_item.append(item_link)
-
-            print(*list_item[0:5])
+        for element in result_links:
+            item_link = element.get_attribute('href')
+            print(item_link)
 
         # assert self.browser.find_element(*YandexLocators.SEARCH_RESULTS_NUM), 'tensor.ru отсутствует'
