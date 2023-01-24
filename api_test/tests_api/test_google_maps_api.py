557f546aed1fff1f3883345715e43e61555bb2b9
nikita1314 <nikita1314@yandex.ru>
2023-01-24 23:16:50 +0300

    Построен проект по автоматизации api, содержащий методы проверки запросов/ответов с логгированием и подключением allure отчёта на примере CRUD теста google maps api

---
+++ api_test/tests_api/test_google_maps_api.py
@@ -0,0 +1,63 @@
+import allure
+import json
+
+from requests import Response
+
+from api_test.utils.checking import Checking
+from api_test.utils.api import Google_maps_api
+
+"""Создание, изменение и удаление новой локации"""
+@allure.epic("Test create new location")
+class Test_create_place():
+
+    @allure.description("Test create, update, delete new place")
+    def test_create_new_place(self):
+        print()
+        print("Метод POST google maps")
+
+        result_post:  Response = Google_maps_api.create_new_place()
+        check_post = result_post.json()
+        place_id = check_post.get("place_id")
+        Checking.check_status_code(result_post, 200)
+        Checking.check_json_token(result_post, ['status', 'place_id', 'scope', 'reference', 'id'])
+        token = json.loads(result_post.text)
+        # print(list(token))
+        Checking.check_json_value(result_post, 'status', 'OK')
+
+        print("Метод GET POST google maps")
+        result_get: Response = Google_maps_api.get_new_place(place_id)
+        Checking.check_status_code(result_get, 200)
+        Checking.check_json_token(result_get, ['location', 'accuracy', 'name', 'phone_number', 'address', 'types', 'website', 'language'])
+        Checking.check_json_value(result_get, 'address', '29, side layout, cohen 09')
+
+
+        print("Метод PUT google maps")
+        result_put: Response = Google_maps_api.put_new_place(place_id)
+        Checking.check_status_code(result_put, 200)
+        Checking.check_json_token(result_put, ['msg'])
+        Checking.check_json_value(result_put, 'msg', 'Address successfully updated')
+
+
+        print("Метод GET PUT google maps")
+        result_get: Response = Google_maps_api.get_new_place(place_id)
+        Checking.check_status_code(result_get, 200)
+        Checking.check_json_token(result_get,
+                                  ['location', 'accuracy', 'name', 'phone_number', 'address', 'types', 'website',
+                                   'language'])
+        Checking.check_json_value(result_get, 'address', '100 Lenina street, RU')
+
+
+        print("Метод DELETE google maps")
+        result_delete: Response = Google_maps_api.delete_new_place(place_id)
+        Checking.check_status_code(result_delete, 200)
+        Checking.check_json_token(result_delete, ['status'])
+        Checking.check_json_value(result_delete, 'status', 'OK')
+
+
+        print("Метод GET DELETE google maps")
+        result_get: Response = Google_maps_api.get_new_place(place_id)
+        Checking.check_status_code(result_get, 404)
+        Checking.check_json_token(result_get, ['msg'])
+        Checking.check_json_search_word_in_value(result_get, 'msg', 'failed')
+
+        print("Тестирование CRUD google maps новой локации прошло успешно!!")
